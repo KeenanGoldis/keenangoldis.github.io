f68bc1146c17f727468141c60d47d92f6ad34268
Keenan Goldis <keenangoldis@gmail.com>
2017-04-08 21:23:35 -0500

    resumeJavascript

---
+++ 01week/resume/js/javascript.js
@@ -0,0 +1,89 @@
+src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js">
+
+
+// Education Section
+
+// This unhides the text when overing over img
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#eduinfo").hide();
+      });
+      $("#codeimgedu").hover(function(){
+          $("#eduinfo").show('slideIn');
+      });
+    });
+
+//this hides the image when hovered over
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#codeimgedu").show();
+      });
+      $("#codeimgedu").hover(function(){
+          $("#codeimgedu").hide('slideIn');
+      });
+    });
+// Work Experience Section
+
+// This unhides the text when overing over img
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#workexperienceinfo").hide();
+      });
+      $("#codeimgwork").hover(function(){
+          $("#workexperienceinfo").show('slideIn');
+      });
+    });
+
+//this hides the image when hovered over
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#codeimgwork").show();
+      });
+      $("#codeimgwork").hover(function(){
+          $("#codeimgwork").hide('slideIn');
+      });
+    });
+
+// Web Dev Info Section
+
+// This unhides the text when overing over img
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#webdevinfo").hide();
+      });
+      $("#codeimg").hover(function(){
+          $("#webdevinfo").show('slideIn');
+      });
+    });
+
+//this hides the image when hovered over
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#codeimg").show();
+      });
+      $("#codeimg").hover(function(){
+          $("#codeimg").hide('slideIn');
+      });
+    });
+
+// Transferable Skills Section
+
+// This unhides the text when overing over img
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#transinfo").hide();
+      });
+      $("#codeimg2").hover(function(){
+          $("#transinfo").show('slideIn');
+      });
+    });
+
+//this hides the image when hovered over
+  $(document).ready(function(){
+      $(document).ready(function(){
+          $("#codeimg2").show();
+      });
+      $("#codeimg2").hover(function(){
+          $("#codeimg2").hide('slideIn');
+      });
+    });
